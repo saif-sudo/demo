0d4a14e51cc3dd3f064f5489476c983cf192cc0c
saif-sudo <mdalisaif4@gmail.com>
2022-02-05 14:32:27 +0600

    first demo commit

---
+++ resources/views/home/login.php
@@ -0,0 +1,7 @@
+<html>
+<form action="" method="">
+        <input type="text" placeholder="Username"><br>
+        <input type="password" placeholder="Password"><br>
+        <input type="submit" >
+    </form>
+</html>
